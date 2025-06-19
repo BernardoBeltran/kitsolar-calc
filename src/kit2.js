fef2b687696b1ee1b0f3cf3d9ca0815728fba123
Ing Beltran <ber100023@gmail.com>
2025-06-19 18:25:40 -0500

    Se corrigen aspectos de diseño y se agrega funcionalidad ver detalles al obtener los resultados

--- src/kit2.js
+++ src/kit2.js
@@ -20,20 +20,20 @@ $d.addEventListener("DOMContentLoaded", () => {
     const $consumo_mensual_ajustado_element = $d.getElementById("consumo_mensual_ajustado_res")
     const $consumo_diario_ajustado_element = $d.getElementById("consumo_diario_ajustado_res");
     const $energia_diaria_generada_por_panel_element = $d.getElementById("energia_diaria_generada_por_panel_res");
-    const $cantidad_de_paneles_necesarios_element = $d.getElementById("cantidad_de_paneles_necesarios_res");const $potencia_total_del_sistema_element = $d.getElementById("potencia_total_del_sistema_res");
+    const $cantidad_de_paneles_necesarios_element = $d.getElementById("cantidad_de_paneles_necesarios_res"); const $potencia_total_del_sistema_element = $d.getElementById("potencia_total_del_sistema_res");
     const $area_estimada_requerida_element = $d.getElementById("area_estimada_requerida_res");
     const $capacidad_total_de_baterias_requerida_element = $d.getElementById("capacidad_total_de_baterias_requerida_res");
     const $kit_adecuado_element = $d.getElementById("kit_adecuado");
 
-
     const $error_text_element = $d.getElementById("error_text")
+
 
     $formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
         let consumo_mensual = parseFloat($consumo_ingresado.value);
 
-        if (isNaN(consumo_mensual)) {
+        if (isNaN(consumo_mensual) || consumo_mensual === "") {
             // $consumo_mensual_element.textContent = "El valor ingresado no es valido"
             $error_text_element.classList.remove("error-text-oculto")
             $error_text_element.classList.add("error-text")
@@ -56,30 +56,30 @@ $d.addEventListener("DOMContentLoaded", () => {
             (consumo_diario_ajustado / energia_diaria_generada_por_panel).toFixed(2)
         )
         potencia_total_del_sistema = parseFloat(
-            (cantidad_de_paneles_necesarios * (potencia_por_panel/1000)).toFixed(2)
+            (cantidad_de_paneles_necesarios * (potencia_por_panel / 1000)).toFixed(2)
         );
         area_estimada_requerida = parseFloat(
             (cantidad_de_paneles_necesarios * 2.4).toFixed(2)
         );
         capacidad_total_de_baterias_requerida = parseFloat(
-            (consumo_diario_ajustado/0.8).toFixed(2)
+            (consumo_diario_ajustado / 0.8).toFixed(2)
         );
 
         const kitAdecuado = (cantidad_de_paneles_necesarios) => {
             if (cantidad_de_paneles_necesarios <= 3) {
-                return "Kit 1";
+                return "1";
             } else if (cantidad_de_paneles_necesarios <= 4) {
-                return "Kit 2";
+                return "2";
             } else if (cantidad_de_paneles_necesarios <= 6) {
-                return "Kit 3";
+                return "3";
             } else if (cantidad_de_paneles_necesarios <= 8) {
-                return "Kit 4";
+                return "4";
             } else {
-                return "Kit 5";
+                return "5";
             }
         };
 
-        
+
         // Interacción DOM
         $consumo_mensual_ajustado_element.textContent = consumo_mensual_ajustado;
         $consumo_diario_ajustado_element.textContent = consumo_diario_ajustado;
@@ -102,7 +102,6 @@ $d.addEventListener("DOMContentLoaded", () => {
         const items = $resultado.querySelectorAll("#resultado-item, #resultdo-item");
         items.forEach((item, i) => item.style.setProperty("--i", i));
 
-       
     });
 
 
